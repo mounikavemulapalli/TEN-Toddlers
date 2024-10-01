f0bb07978722d6250c28ca9b3519c2a241a5a014
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-10-01 10:46:58 +0530

    Created ApplicationForm Component

---
+++ Frontend/src/assets/data/careerAbout.js
@@ -0,0 +1,26 @@
+export const careerAbout = [
+  {
+    id: 0,
+    image: "https://www.uptodd.com/images/career/why-join-icon1.svg",
+    name: "Role With a purpose",
+    desc: "Role with an opportunity to nurture Future Generation",
+  },
+  {
+    id: 1,
+    image: "https://www.uptodd.com/images/career/why-join-icon2.svg",
+    name: "Impact at Work",
+    desc: "Empower every moment with the magic of growth, transforming tiny steps into giant leaps for the future's architects.",
+  },
+  {
+    id: 2,
+    image: "https://www.uptodd.com/images/career/why-join-icon3.svg",
+    name: "Collaborative culture",
+    desc: "Easily approachable and cooperative team with no air around designations",
+  },
+  {
+    id: 3,
+    image: "https://www.uptodd.com/images/career/why-join-icon4.svg",
+    name: "Vision-Oriented Team",
+    desc: "Join a constellation of dreamers and doers, united by a vision to unlock the infinite potential of every child",
+  },
+];
---
+++ Frontend/src/assets/data/teamCards.js
@@ -0,0 +1,32 @@
+export const teamCards = [
+  {
+    id: 0,
+    image: "https://www.uptodd.com/images/career/academics.webp",
+    name: "Product Team",
+    desc: "Crafting tomorrow's innovations today, our Product Team weaves technology and empathy into solutions that redefine parenting",
+  },
+  {
+    id: 1,
+    image: "https://www.uptodd.com/images/career/developer.webp",
+    name: "Tech Team",
+    desc: "Empowering the future of parenting with cutting-edge technology, our Tech Team is the heartbeat of innovation at UpTodd",
+  },
+  {
+    id: 2,
+    image: "https://www.uptodd.com/images/career/branding.webp",
+    name: "Marketing Team",
+    desc: "Shaping narratives and connecting hearts, our Marketing Team masterfully spreads the UpTodd story, turning insights into impact",
+  },
+  {
+    id: 3,
+    image: "https://www.uptodd.com/images/career/sales.webp",
+    name: "Sales Team",
+    desc: "Driving growth and forging paths, our Business Team navigates opportunities to elevate UpTodd on the global stage",
+  },
+  {
+    id: 4,
+    image: "https://www.uptodd.com/images/career/contents.webp",
+    name: "Content Team",
+    desc: "Crafting the voice of tomorrow, our Content Team blends creativity with strategy to inspire and educate parents at every step",
+  },
+];
