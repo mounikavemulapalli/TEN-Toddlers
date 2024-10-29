9f234719a45c6cdf4f48f5c36a9c2b02333b10a2
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-10-29 10:57:45 +0530

    changes done

--- Frontend/src/assets/data/parentsData.js
+++ Frontend/src/assets/data/parentsData.js
@@ -1,4 +1,8 @@
-import baby1 from "../../assets/images/subscription/baby1.avif";
+import baby1 from "../../assets/images/subscription/baby1.webp";
+import baby2 from "../../assets/images/subscription/baby2.jpg";
+import baby3 from "../../assets/images/subscription/baby3.jpg";
+import baby4 from "../../assets/images/subscription/baby4.jpg";
+import baby5 from "../../assets/images/subscription/baby5.webp";
 
 export const parentsData = [
   {
@@ -11,7 +15,7 @@ export const parentsData = [
   },
   {
     id: 1,
-    image: baby1,
+    image: baby2,
     name: "Aahana, 2 Years and 8 Months Old",
     about:
       "We found out that our daughter is not active at all, neither in terms of physical activity nor she likes the presence of anyone apart from me. But this kit has provided exceptional support to me in the journey and its just wow.",
@@ -19,7 +23,7 @@ export const parentsData = [
   },
   {
     id: 2,
-    image: baby1,
+    image: baby3,
     name: "Miss Sandhya's baby, 1.5 Weeks Old",
     about:
       "UpTodd kit is just great, I really liked the quality of materials and scientific aspects behind it. Overall package looks very complete as every essentials are covered.",
@@ -27,7 +31,7 @@ export const parentsData = [
   },
   {
     id: 3,
-    image: baby1,
+    image: baby4,
     name: "Aesha, 1.5 Years old",
     about:
       "Its much better than any kit purchased by me till date, best part is the linkage of kit with the dashboard is just amazing. Overall its amazing with milestones, diet and parenting intro program.",
@@ -35,7 +39,7 @@ export const parentsData = [
   },
   {
     id: 4,
-    image: baby1,
+    image: baby5,
     name: "Faria, 6 Months Old",
     about:
       "Thanks to my Dr Pradeep for suggesting such an amazing kit, my girl is doing excellent now in all the terms. This is wonderful kit with every thing arranged and provided in the best shape.",
