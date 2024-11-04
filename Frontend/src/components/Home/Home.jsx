500c7cfe42570ba44a3f7a3e06d12e6c1d590bf6
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-11-04 19:59:12 +0530

    premium page all things done

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -3,14 +3,13 @@ import { FaCheckCircle } from "react-icons/fa";
 import Button from "../../components/Home/HomeButton.jsx";
 import Slider from "../comman/Slider.jsx";
 import Peek from "../comman/Peek.jsx";
+import GeniusProgram from "../comman/GeniusProgram.jsx";
 import BrilliantMinds from "../comman/BrilliantMinds.jsx";
 import Line from "../comman/Line.jsx";
 import HappyParents from "../comman/HappyParents.jsx";
 import GlobalExperts from "../comman/GlobalExperts.jsx";
 import SuperScience from "../comman/SuperScience.jsx";
 import Mentors from "../comman/Mentors.jsx";
-import Modal from "../Modal.jsx";
-import { premiumProgram } from "../../assets/Lists/premiumProgram.js";
 import { possibilitesList } from "../../assets/data/possibilitesList.js";
 import { pillarsList } from "../../assets/Lists/pillarsList.js";
 import { boardMembers } from "../../assets/Lists/boardMembers.js";
@@ -299,92 +298,7 @@ export default function Home() {
         </section>
 
         {/* UpTodd Genius Premium Program Section */}
-        <section className="product-and-cost">
-          <div className="product-and-cost-header">
-            <h1>UpTodd™ Genius Premium Program</h1>
-            <h5>
-              Exclusive Parenting Program for Overall Baby Brain Development
-            </h5>
-            <h5>
-              Exclusive Mega KIT + World's Only True Personalised APP + Brain
-              Dev. Report & Support from World's Top Experts
-            </h5>
-          </div>
-          <ul className="product-and-cost-container">
-            <li>
-              <FaCheckCircle /> <span>Dedicated R&D Team Care</span>
-            </li>
-            <li>
-              <FaCheckCircle /> <span>Research Backed Growth System</span>
-            </li>
-            <li>
-              <FaCheckCircle /> <span>Personalised Organic Mega Kit</span>
-            </li>
-            <li>
-              <FaCheckCircle /> <span>One-Stop Personalised APP</span>
-            </li>
-            <li>
-              <FaCheckCircle /> <span>Holistic Tracking & Brain Reports</span>
-            </li>
-            <li>
-              <FaCheckCircle /> <span>Recognitions & Certifications</span>
-            </li>
-          </ul>
-          <div className="product-and-cost-container-card">
-            {premiumProgram.map((card, index) => (
-              <div key={index} className="product-and-cost-card">
-                <h1>{card.text1}</h1>
-                <h2>{card.text2} </h2>
-                <p>{card.text3} </p>
-                <div className="card-perMonth">
-                  <span>{card.perMonth}</span>
-                </div>
-                <h3>{card.discountPrice}</h3>
-                <del>{card.deletePrice} </del>
-
-                {/* <Button text='Enroll Now' /> */}
-                <PopupButton text="Enroll Now" />
-              </div>
-            ))}
-          </div>
-          <div className="program-test">
-            <p>
-              Want to know more about program
-              <span onClick={openModal}>see here</span>
-            </p>
-            <Modal show={showModal} onClose={closeModal}></Modal>
-          </div>
-        </section>
-
-        {/* 2 card Section  */}
-        <section className="developmental-kit">
-          <div className="kit-intro">
-            <div>
-              <h3>Curated Just for You: Mega Kit at Your Doorstep</h3>
-              <p>World's only true personalised system</p>
-            </div>
-            <img
-              src="https://www.uptodd.com/images/newWebsite/kit-shipment.svg"
-              loading="lazy"
-              alt="Personalised Baby Kit"
-            />
-          </div>
-          <div className="kit-video">
-            <iframe
-              loading="lazy"
-              srcDoc="<style>body,.full {width:100%;height:100%;margin:0;position:absolute;display:flex;justify-content:center;object-fit:cover}</style> <a href='https://www.youtube.com/embed/r1SwlfmvuCM?autoplay=1;modestbranding=1&amp;autohide=1&amp;rel=0' className='full'><img src='https://www.uptodd.com/images/newWebsite/kit-video.webp' className='full' /><svg xmlns='http://www.w3.org/2000/svg' width='46' height='46' viewBox='0 0 46 46' fill='none' style='position: relative; left:3%;top:30%;'><mask id='mask0_303_1345' style='mask-type:alpha' maskUnits='userSpaceOnUse' x='0' y='0' width='46' height='46'><rect width='45.9356' height='45.9356' fill='#D9D9D9'/></mask><g mask='url(#mask0_303_1345)'><path d='M18.1828 31.5807L31.5807 22.9677L18.1828 14.3548L18.1828 31.5807ZM22.9677 42.1076C20.32 42.1076 17.8319 41.6051 15.5032 40.6003C13.1745 39.5955 11.1489 38.2318 9.42628 36.5092C7.7037 34.7866 6.33999 32.7609 5.33514 30.4323C4.3303 28.1036 3.82788 25.6154 3.82788 22.9677C3.82788 20.32 4.3303 17.8319 5.33514 15.5032C6.33999 13.1745 7.7037 11.1489 9.42628 9.42628C11.1489 7.7037 13.1745 6.33999 15.5032 5.33514C17.8319 4.3303 20.32 3.82788 22.9677 3.82788C25.6154 3.82788 28.1036 4.3303 30.4323 5.33514C32.7609 6.33999 34.7866 7.7037 36.5092 9.42628C38.2318 11.1489 39.5955 13.1745 40.6003 15.5032C41.6051 17.8319 42.1076 20.32 42.1076 22.9677C42.1076 25.6154 41.6051 28.1036 40.6003 30.4323C39.5955 32.7609 38.2318 34.7866 36.5092 36.5092C34.7866 38.2318 32.7609 39.5955 30.4323 40.6003C28.1036 41.6051 25.6154 42.1076 22.9677 42.1076Z' fill='white'/></g></svg> </a>"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen=""
-              data-gtm-yt-inspected-4="true"
-              data-gtm-yt-inspected-40611628_62="true"
-              id="72071507"
-              data-gtm-yt-inspected-14="true"
-            ></iframe>
-            <h3>SAMPLE Personalised KIT</h3>
-          </div>
-        </section>
+        <GeniusProgram />
 
         {/*  Brilliant Minds Section */}
         <BrilliantMinds />
