e48bf62d3eeaab62e7f21908c12062a5c0413b5c
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-11-08 18:29:34 +0530

    parents component paraphrase done

--- Frontend/src/assets/data/parentsData.js
+++ Frontend/src/assets/data/parentsData.js
@@ -8,41 +8,41 @@ export const parentsData = [
   {
     id: 0,
     image: baby1,
-    name: "Kedar, 7 Months Old",
+    name: "Arush, 7 Months Old",
     about:
-      "I am super happy with kit, and its highly recommended for all parents. Things which I liked most are dashboard tutorials, high quality finish in the materials, milestone tracker and diet plan. Its completely worth it to the investment I made, at this cost nothing can be better than this.",
-    parent: "- Kedar's Mother",
+      "I'm extremely satisfied with this kit and highly recommend it to all parents. My favorite aspects are the dashboard tutorials, the high-quality materials, the milestone tracker, and the diet plan. It's absolutely worth the investment—I couldn’t find anything better at this price.",
+    parent: "- Arush's Mother",
   },
   {
     id: 1,
     image: baby2,
-    name: "Aahana, 2 Years and 8 Months Old",
+    name: "Vikas, 2 Years and 8 Months Old",
     about:
-      "We found out that our daughter is not active at all, neither in terms of physical activity nor she likes the presence of anyone apart from me. But this kit has provided exceptional support to me in the journey and its just wow.",
-    parent: "- Aahana's Mother",
+      "We discovered that our daughter isn’t very active, both physically and socially—she doesn’t enjoy being around anyone except me. But this kit has been an incredible support for us on this journey, and it’s simply amazing.",
+    parent: "- Vikas's Mother",
   },
   {
     id: 2,
     image: baby3,
-    name: "Miss Sandhya's baby, 1.5 Weeks Old",
+    name: "Miss Komal's baby, 1.5 Weeks Old",
     about:
-      "UpTodd kit is just great, I really liked the quality of materials and scientific aspects behind it. Overall package looks very complete as every essentials are covered.",
-    parent: "- Miss Sandhya",
+      "The UpTodd kit is fantastic! I truly appreciated the quality of the materials and the scientific principles behind it. The entire package feels very comprehensive, as it covers all the essentials.",
+    parent: "- Miss Komal",
   },
   {
     id: 3,
     image: baby4,
-    name: "Aesha, 1.5 Years old",
+    name: "Tarun, 1.5 Years old",
     about:
-      "Its much better than any kit purchased by me till date, best part is the linkage of kit with the dashboard is just amazing. Overall its amazing with milestones, diet and parenting intro program.",
-    parent: "- Aesha’s Father",
+      "This kit is far better than any I've bought so far. The integration with the dashboard is incredible. Overall, it's fantastic, especially with the milestones, diet plan, and parenting introduction program.",
+    parent: "- Tarun’s Father",
   },
   {
     id: 4,
     image: baby5,
-    name: "Faria, 6 Months Old",
+    name: "Shiva, 6 Months Old",
     about:
-      "Thanks to my Dr Pradeep for suggesting such an amazing kit, my girl is doing excellent now in all the terms. This is wonderful kit with every thing arranged and provided in the best shape.",
-    parent: "- Faria’s Mother",
+      "I’m so grateful to Dr. Pradeep for recommending this amazing kit. My daughter is thriving in every way now. It’s a fantastic kit, with everything perfectly organized and provided in the best condition.",
+    parent: "- Shiva’s Mother",
   },
 ];
