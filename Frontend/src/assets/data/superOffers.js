ba0ce2e7d46d479c46948c15e17a571f426cefbc
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-11-09 18:16:47 +0530

    content changed

--- Frontend/src/assets/data/superOffers.js
+++ Frontend/src/assets/data/superOffers.js
@@ -3,44 +3,44 @@ export const superOffers = [
     id: 0,
     image:
       "https://www.uptodd.com/images/newWebsite/why-uptodd/why-uptodd-1.webp",
-    name: "Holistic Framework: Nurturing Genius",
-    point1: "Brain stimulating Music",
-    point2: "Screen-Free Activities",
+    name: "Comprehensive Framework for Genius Development",
+    point1: "Music to Stimulate Brain Growth",
+    point2: "Activities Without Screen Time",
   },
   {
     id: 1,
     image:
       "https://www.uptodd.com/images/newWebsite/why-uptodd/why-uptodd-2.webp",
-    name: "Milestone Tracker: Development Reports",
-    point1: "Personalized Brain Development Insights",
-    point2: "Celebrate one step at a time",
+    name: "Track Growth Milestones with Development Reports",
+    point1: "Tailored Insights for Brain Development",
+    point2: "Celebrate Progress Every Step of the Way",
   },
   {
     id: 2,
     image:
       "https://www.uptodd.com/images/newWebsite/why-uptodd/why-uptodd-3.webp",
-    name: "Expert-Led Webinars for You & Baby",
-    point1: "Exclusive Mastersession",
-    point2: "Interactive Webinar with Expert",
+    name: "Webinars Led by Experts for You and Your Child",
+    point1: "Exclusive Masterclass Sessions",
+    point2: "Interactive Expert-led Webinars",
   },
   {
     id: 3,
     image:
       "https://www.uptodd.com/images/newWebsite/why-uptodd/why-uptodd-4.webp",
-    name: "Personalized Support: Parental Coaching",
-    point1: "Expert Parental Coaches at Your Fingertips",
-    point2: "High-Priority Access to Technical Support",
+    name: "Customized Support Through Parental Coaching",
+    point1: "Access to Skilled Parenting Coaches",
+    point2: "Priority Access for Technical Help",
   },
   {
     id: 4,
     image:
       "https://www.uptodd.com/images/newWebsite/why-uptodd/why-uptodd-5.webp",
-    name: "Backed by Research & Science",
-    point1: "Parenting Skills Training Session",
-    point2: "Daily Development Journey",
+    name: "Grounded in Scientific Research",
+    point1: "Sessions to Develop Parenting Skills",
+    point2: "Daily Insights into Developmental Growth",
   },
   {
     id: 5,
-    name: "Expert Tips at Your Fingertips",
+    name: "Expert Advice Anytime",
   },
 ];
