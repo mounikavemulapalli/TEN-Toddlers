7d1ba3a32cd1b742c14d7b2faf2649749e6ae4ba
Anurag Pardeshi <pardeshianurag22@gmail.com>
2024-11-11 17:17:10 +0530

    Mentors Section updated

--- Frontend/src/assets/data/superMentors.js
+++ Frontend/src/assets/data/superMentors.js
@@ -2,39 +2,137 @@ export const superMentors = [
   {
     id: 0,
     image:
-      "https://www.uptodd.com/images/newWebsite/professors/krishna_vedula.webp",
-    alt: "Uptodd mentor Prof.Krishna Vedula MIT",
-    name: "Krishna Vedula",
-    prof: "Professor, MIT, United States",
+      "src/assets/images/TenMentors/Bhagyashree.jpeg",
+    alt: "Uptodd mentor Bhagyashree Patil",
+    name: "Bhagyashree Patil",
+    prof: "A mentor at TEN, she excels in React.js, Java, HTML, CSS, and JavaScript, guiding software engineering teams effectively.",
   },
   {
     id: 1,
     image:
-      "https://www.uptodd.com/images/newWebsite/professors/jaideep_sharma.webp",
-    alt: "Uptodd mentor Dr.Jaideep Sharma AIIMS",
-    name: "Dr. Jaideep Sharma",
-    prof: "MBBS & MD, AIIMS, New Delhi",
+      "src/assets/images/TenMentors/Srikrupa.jpeg",
+    alt: "Uptodd mentor Srikrupa.jpeg",
+    name: "Srikrupa.jpeg",
+    prof: "Experienced in data science, specializing in multivariable regression and web scraping using Python, Pandas, and BeautifulSoup.",
   },
   {
     id: 2,
-    image: "https://www.uptodd.com/images/newWebsite/professors/sudhanshu.webp",
-    alt: "Uptodd mentor Dr.Sudhanshu Singh",
-    name: "Dr. Sudhanshu",
-    prof: "MD, DSMA CMC Vellore",
+    image:
+      "src/assets/images/TenMentors/Aryan.jpeg",
+    alt: "Uptodd mentor Aryan Panchal",
+    name: "Aryan Panchal",
+    prof: "Full-stack developer with AI/ML expertise, pursuing a computer engineering degree. Skilled in front-end and back-end.",
   },
   {
     id: 3,
     image:
-      "https://www.uptodd.com/images/newWebsite/professors/manoj_mondal.webp",
-    alt: "Uptodd mentor Prof.Manoj Mondal IIT KGP",
-    name: "Manoj Mondal",
-    prof: "Professor, IIT Kharagpur",
+      "src/assets/images/TenMentors/Piush.jpeg",
+    alt: "Uptodd mentor Piush Bose",
+    name: "Piush Bose",
+    prof: "A mentor at TEN, she excels in React.js, Java, HTML, CSS, and JavaScript, guiding software engineering teams effectively.",
   },
   {
     id: 4,
-    image: "https://www.uptodd.com/images/newWebsite/professors/PK_Mishra.webp",
-    alt: "Uptodd mentor PK Mishra IIT BHU",
-    name: " Prof. PK Mishra",
-    prof: "IIT BHU",
+    image:
+      "src/assets/images/TenMentors/Faizan.jpeg",
+    alt: "Uptodd mentor Faizan Surani",
+    name: "Faizan Surani",
+    prof: "A skilled Full Stack Developer, he specializes in JavaScript, React, Node.js, Express, and MongoDB technologies.",
   },
+  {
+    id: 5,
+    image:
+      "src/assets/images/TenMentors/Pooja.jpeg",
+    alt: "Uptodd mentor Ms Pooja Lalan",
+    name: "Ms Pooja Lalan",
+    prof: "Experienced AWS trainer and Cloud Consultant with 15 years' expertise in technology, strategy, and AWS Cloud.",
+  },
+  {
+    id: 6,
+    image:
+      "/src/assets/images/TenMentors/Amar.jpeg",
+    alt: "Uptodd mentor Amar Kumar",
+    name: "Amar Kumar",
+    prof: "Passionate coder with expertise in Java, C++, DSA, and MERN stack, contributing to impactful web development projects.",
+  },
+  {
+    id: 7,
+    image:
+      "src/assets/images/TenMentors/Sachin.jpeg",
+    alt: "Uptodd mentor Sachin Chaudhary",
+    name: "Sachin Chaudhary",
+    prof: "Data science leader specializing in automotive, manufacturing, healthcare, insurance, mentoring career transitions, and fostering AI learning paths.",
+  },
+  {
+    id: 8,
+    image:
+      "src/assets/images/TenMentors/Avinash.jpeg",
+    alt: "Uptodd mentor Avinash Karve",
+    name: "Avinash Karve",
+    prof: "IT leader managing SAP projects, team development, specializing in project management and professional mentoring.",
+  },
+  {
+    id: 9,
+    image:
+      "src/assets/images/TenMentors/Abhishek.jpeg",
+    alt: "Uptodd mentor Abhishek Kumar",
+    name: "Abhishek Kumar",
+    prof: "With 7+ years of experience, I led full-stack development, project management, and backend systems for companies like Dody Dev and UrbanClap.",
+  },
+  {
+    id: 10,
+    image:
+      "src/assets/images/TenMentors/Afif.jpeg",
+    alt: "Uptodd mentor Afif Patel",
+    name: "Afif Patel",
+    prof: "Senior Engineer with 7 years' experience in IT infrastructure, security, Azure Cloud, device management, automation, and IT operations improvement",
+  },
+  {
+    id: 11,
+    image:
+      "src/assets/images/TenMentors/PoojaChopra.jpeg",
+    alt: "Uptodd mentor Pooja Chopra",
+    name: "Pooja Chopra",
+    prof: "I have expertise in Java Full Stack training and development, covering frontend, backend, databases, and frameworks for comprehensive solutions.",
+  },
+  {
+    id: 12,
+    image:
+      "src/assets/images/TenMentors/Saurabh.jpeg",
+    alt: "Uptodd mentor Saurabh Gupta",
+    name: "Saurabh Gupta",
+    prof: "Chief Product & Tech Officer at FroGo with 18+ years of experience in digital products, data-driven transformation, leadership, and entrepreneurship.",
+  },
+  {
+    id: 13,
+    image:
+      "src/assets/images/TenMentors/Ayan.jpeg",
+    alt: "Uptodd mentor Ayan Kumar Ghosh",
+    name: "Ayan Kumar Ghosh",
+    prof: "With 10 years' R&D experience, I specialize in microelectronics, machine learning, quantum technologies, and quantum device fabrication for AI applications.",
+  },
+  {
+    id: 14,
+    image:
+      "src/assets/images/TenMentors/Harshadi.jpeg",
+    alt: "Uptodd mentor Harshadi Hansora",
+    name: "Harshadi Hansora",
+    prof: "With 6+ years' experience in software development and teaching, I specialize in React, Angular, TypeScript, Node.js, Python, and more.",
+  },
+  {
+    id: 15,
+    image:
+      "src/assets/images/TenMentors/Sonia.jpeg",
+    alt: "Uptodd mentor Sonia Malik",
+    name: "Sonia Malik",
+    prof: "I specialize in conducting live and recorded sessions on Data Structures, Algorithms, C, C++, JavaScript, HTML, CSS, Git, and more.",
+  },
+  {
+    id: 16,
+    image:
+      "src/assets/images/TenMentors/Premji.jpeg",
+    alt: "Uptodd mentor Premji B",
+    name: "Premji B",
+    prof: "Premji Boominathan, a seasoned leader with 26 years in agile product management, excels in driving innovation, team development, and strategic growth.",
+  }, 
 ];
